3106437e688008a65df85c6e45bdf7286eee5b9b
myung <ksmrva@gmail.com>
2024-07-29 01:03:32 -0400

    -Moving gitignore back to top level directory -Creating unit testing skeleton -Creating build file to build both Api and UnitTest projects at the same time

---
+++ HouseInv.UnitTests/NotesControllerTests.cs
@@ -0,0 +1,17 @@
+namespace HouseInv.UnitTests;
+
+public class NotesControllerTests
+{
+    [Fact]
+    public void GetNoteAsync_WithUnexistingNote_ReturnsNotFound()
+    {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+    }
+}
